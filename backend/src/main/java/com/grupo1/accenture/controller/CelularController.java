429941259d5b901eb259ee81e1e94f40d52a2261
Gustavo Maciel <gustamaciel2012@hotmail.com>
2020-10-17 16:48:34 -0300

    Atualizando o Controller

--- backend/src/main/java/com/grupo1/accenture/controller/CelularController.java
+++ backend/src/main/java/com/grupo1/accenture/controller/CelularController.java
@@ -5,10 +5,12 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.annotation.WebServlet;
 
 /**
  * Servlet implementation class CelularController
  */
+@WebServlet(description = "Trata os dados recebidos pelo front-end do celular", urlPatterns = { "/enviar" })
 public class CelularController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -25,7 +27,7 @@ public class CelularController extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.getWriter().append("CelularController");
 	}
 
 	/**
