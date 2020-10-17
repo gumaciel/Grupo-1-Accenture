597c7e065d4c6f4ef861a3a3bd187210c06a3f49
Henrique Cunha <henrique.cunha@corpus.inf.br>
2020-10-17 16:34:49 -0300

    teclado primeira versao

--- front/desafioaccenture/src/App.js
+++ front/desafioaccenture/src/App.js
@@ -1,23 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="container">
+          <div className="content">
+            <div className="btn-group">
+              <button className="btn">1</button>
+              <button className="btn">2</button>
+              <button className="btn">3</button>
+            </div>
+            <div className="btn-group">
+              <button className="btn">4</button>
+              <button className="btn">5</button>
+              <button className="btn">6</button>
+            </div>
+            <div className="btn-group">
+              <button className="btn">7</button>
+              <button className="btn">8</button>
+              <button className="btn">9</button>
+            </div>
+          </div>
+        </div>
       </header>
     </div>
   );
